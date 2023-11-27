dff0094b57eb36fceeffee29b00f2e487009d1f1
qjdaquino <qjdaquino@tip.edu.ph>
2023-11-27 17:02:11 +0800

    lagay ng lens id

--- example/lib/constants.dart
+++ example/lib/constants.dart
@@ -3,14 +3,16 @@
 class Constants {
   /// List of group IDs for Camera Kit (TODO: Fill group ID here).
 
-  static const List<String> groupIdList = ['your-group-ids'];
+  static const List<String> groupIdList = [
+    'aabbc32b-099f-405b-b9e0-bc3bf43348db'
+  ];
 
   /// optional: if you want to get single lens you can set it otherwise set empty string
   /// The lens ID for Camera Kit (TODO: Fill lens ID here).
-  static const cameraKitLensId = 'camera-kit-lens-id';
+  static const cameraKitLensId = '9e851ce3-1a99-41c6-ba46-38ef38034c18';
 
   /// The API token for Camera Kit in the staging environment (TODO: Fill API token here).
 
   static const cameraKitApiToken =
-      'your-api-token'; // TODO fill api token staging or production here
+      'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNjg2ODEwMjI2LCJzdWIiOiJhZDNjNDYzNy0xYzNkLTRjODEtODlkMC02NmMyZTc5OThiZTN-U1RBR0lOR342NmIyNDQzZS02MzJlLTRhYTktOTUxYS0yZTdiYmE0YmJlNDMifQ.UvyUspg7jLoD_UrYgt2Gru_NgrA9UwZfZKGAHYtI7eI'; // TODO fill api token staging or production here
 }
